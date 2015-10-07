be690c7535a367c0c0a289c2fe11add355369941
beatbreaker82 <beatbreaker82@gmail.com>
2015-10-07 20:55:11 +1100

    Update tweepy_test.py

--- tweepy_test.py
+++ tweepy_test.py
@@ -1,11 +1,3 @@
-# Consumer Key (API Key) seLtRPmpcnCpbGtOzRMkXJz3d
-# Consumer Secret (API Secret) Hlee0nP1W5CwteG6uEHjzcyvLDL833Dvb3H5d8r6SHxaO5kwhW
-# Access Level Read and write (modify app permissions)
-# Owner Michael_J_Andy
-# Owner ID 851859229
-# Access Token 851859229-SNsRCZoCzAaCvu43k7hk094M6wjKm4VsJBjU3TZw
-# Access Token Secret eUboOm2u5cMoWbeVsZkGL6Cy7JN6OsKS1Vr7cmuOgUuqs
-
 import tweepy
 # I've deleted the Twitter App but just in case
 consumer_key = "XXXXXX"
